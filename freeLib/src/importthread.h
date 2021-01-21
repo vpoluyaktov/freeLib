6a6bbe4692f6b54e32732a3651a7dbb7b54570bb
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2021-01-21 09:12:53 +0300

    Improved (ImportThread::importFB2()): Улучшение вывода информации по добавлению в библиотеку книг разных форматов.

--- freeLib/src/importthread.h
+++ freeLib/src/importthread.h
@@ -33,7 +33,7 @@ protected:
     void importFB2(QString path,int &count);
     void importFB2_main(QString path);
 
-    void readFB2(const QByteArray &ba,QString file_name, QString arh_name,qint32 file_size=0);
+    void readFB2(bool isFB2, const QByteArray &ba, QString file_name, QString arh_name, qint32 file_size=0);
     void readEPUB(const QByteArray &ba,QString file_name, QString arh_name,qint32 file_size=0);
     void readFB2_test(const QByteArray& ba,QString file_name,QString arh_name);
 private:
