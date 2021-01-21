38772b5457aedec02e3478b3bf323c853c56ee6f
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2021-01-21 14:12:18 +0300

    Refactoring (ImportThread::importFB2()): Правка комментарие для бОлее правильного восприятия их byajhvfwbb/

--- freeLib/src/importthread.cpp
+++ freeLib/src/importthread.cpp
@@ -689,8 +689,8 @@ void ImportThread::importFB2(QString path, int &count)
                         if(fi.completeBaseName().left(1)!="." && !fi.completeBaseName().isEmpty())
                         {
                             QString fbd = fi.path() != "."
-                                ? fi.path() + "/" + fi.completeBaseName() + ".fbd"  /* zip: файлы в папке*/
-                                : fi.completeBaseName() + ".fbd";                   /* zip: файлы без папки*/
+                                ? fi.path() + "/" + fi.completeBaseName() + ".fbd"  /* файлы в zip в папке*/
+                                : fi.completeBaseName() + ".fbd";                   /* файлы в zip без папки*/
                             if(SetCurrentZipFileName(&uz,fbd))
                             {
                                 SetCurrentZipFileName(&uz,zip_fi.name);
