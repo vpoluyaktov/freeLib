5de09156e1be19c7da942df15724448544ed0297
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2021-01-21 14:33:22 +0300

    Improved (ImportThread::readFB2()): Улучшен вывод формата книг в лог при импорте файлов.

--- freeLib/src/importthread.cpp
+++ freeLib/src/importthread.cpp
@@ -494,7 +494,7 @@ void ImportThread::readFB2_test(const QByteArray& ba,QString file_name,QString a
     AddGenre(id_book,"sf",existingID,language);
 }
 
-void ImportThread::readFB2(bool isFB2, const QByteArray& ba, QString file_name, QString arh_name, qint32 file_size)
+void ImportThread::readFB2(const QByteArray& ba, QString file_name, QString arh_name, qint32 file_size)
 {
     if(arh_name.isEmpty())
     {
@@ -517,10 +517,7 @@ void ImportThread::readFB2(bool isFB2, const QByteArray& ba, QString file_name, 
         return;
     }
 
-    if(isFB2)
-        emit Message(tr("Book add (fb2):") + " " + file_name);
-    else
-        emit Message(tr("Book add (fbd):") + " " + file_name);
+    emit Message(QString(tr("Book add (%1): %2")).arg(fi.suffix(), file_name));
 
     book_info bi;
     GetBookInfo(bi,ba,"fb2",true);
@@ -618,7 +615,7 @@ void ImportThread::importFB2(QString path, int &count)
                 if(file.exists())
                 {
                     file.open(QFile::ReadOnly);
-                    readFB2(false, file.readAll(), file_name, "", iter->size());
+                    readFB2(file.readAll(), file_name, "", iter->size());
                     count++;
                 }
             }
@@ -630,7 +627,7 @@ void ImportThread::importFB2(QString path, int &count)
                 file.open(QFile::ReadOnly);
                 QByteArray ba=file.readAll();
                 if(iter->suffix().toLower()=="fb2")
-                    readFB2(true, ba, file_name, "");
+                    readFB2(ba, file_name, "");
                 else
                     readEPUB(ba,file_name,"");
                 count++;
@@ -674,7 +671,7 @@ void ImportThread::importFB2(QString path, int &count)
                         zip_file.open(QIODevice::ReadOnly);
                         buffer.setData(zip_file.read(16*1024));
                         zip_file.close();
-                        readFB2(true, buffer.data(), str.name, file_name, str.uncompressedSize);
+                        readFB2(buffer.data(), str.name, file_name, str.uncompressedSize);
                     }
                     else if(zip_fi.name.right(3).toLower()=="epub")
                     {
@@ -696,7 +693,7 @@ void ImportThread::importFB2(QString path, int &count)
                                 SetCurrentZipFileName(&uz,zip_fi.name);
                                 zip_file.open(QIODevice::ReadOnly);
                                 buffer.setData(zip_file.readAll());
-                                readFB2(false, buffer.data(), str.name, file_name, str.uncompressedSize);
+                                readFB2(buffer.data(), str.name, file_name, str.uncompressedSize);
                             }
                         }
                     }
