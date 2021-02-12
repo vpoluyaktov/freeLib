6b81d8b64a47f571eb9ea08a97462aca0419abc1
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2021-02-12 09:21:26 +0300

    Improved (MainWindow::DeleteAllBooksFromGroup()): Сделано удаление всех книг выделенной группы из структур текущей библиотеки.

--- freeLib/src/mainwindow.cpp
+++ freeLib/src/mainwindow.cpp
@@ -3352,6 +3352,45 @@ void MainWindow::AddBookToGroupAction()
 */
 void MainWindow::DeleteAllBooksFromGroup()
 {
-    // чтение из структуры библиотеки всех книг выделенной группы
+    // Формирование списка книг для выделенной Группы
+    QList<uint> listBooks;
+    QHash<uint, SBook>::const_iterator BookConstIterator = mLibs[g_idCurrentLib].mBooks.constBegin();
+    while (BookConstIterator != mLibs[g_idCurrentLib].mBooks.constEnd()) {
+        if (idCurrentLanguage_ == -1 || idCurrentLanguage_ == BookConstIterator->idLanguage) {
+            foreach(uint iGroup, BookConstIterator->listIdGroups) {
+                if (iGroup == idCurrentGroup_) {
+                    listBooks << BookConstIterator.key();
+                    break;
+                }
+            }
+        }
+        ++BookConstIterator;
+    }
     
+    // удаление в базе книг из сформированного списка
+    QSqlQuery query(QSqlDatabase::database("libdb"));
+    foreach(uint book_id, listBooks) {
+        query.prepare("DELETE FROM book_group WHERE id_lib = :id_lib AND group_id = :group_id AND book_id = :book_id;");
+        query.bindValue(":book_id", book_id);
+        query.bindValue(":group_id", idCurrentGroup_);
+        query.bindValue(":id_lib", g_idCurrentLib);
+        if (!query.exec())
+            qDebug() << query.lastError().text();
+    }
+
+    // удаление из структуры связи этой книги с выделенной группой
+    QHash<uint, SBook>::iterator BookIterator = mLibs[g_idCurrentLib].mBooks.begin();
+    while (BookIterator != mLibs[g_idCurrentLib].mBooks.end()) {
+        if (idCurrentLanguage_ == -1 || idCurrentLanguage_ == BookIterator->idLanguage) {
+            QMutableListIterator<uint> GroupIterator(BookIterator->listIdGroups);
+            while (GroupIterator.hasNext()) {
+                if (GroupIterator.next() == idCurrentGroup_) {
+                    GroupIterator.remove();
+                    break;
+                }
+            }
+        }
+        ++BookIterator;
+    }
+    ui->Books->clear();
 }
