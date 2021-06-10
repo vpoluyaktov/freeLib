de07b8425a4412d89e6ccef4d94a6734df3a3b3f
Vadim Kuznetsov (dikbsd) <dikbsd@gmail.com>
2021-06-10 11:41:54 +0300

    Improved (MainWindow::on_comboBoxLanguageFilter_currentIndexChanged()): Фильтрация по языку теперь влияет на отображение в группе числа отфильтрованных книг.

--- freeLib/src/mainwindow.cpp
+++ freeLib/src/mainwindow.cpp
@@ -2979,6 +2979,8 @@ void MainWindow::on_comboBoxLanguageFilter_currentIndexChanged(const QString &ar
     FillListWidgetAuthors(g_idCurrentLib);
     FillListWidgetSerials(g_idCurrentLib);
     FillTreeWidgetGenres(g_idCurrentLib);
+    FillListWidgetGroups(g_idCurrentLib);
+
     FillListBooks();
 }
 
